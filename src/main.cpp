a15345b40dcc51afaeb2ab5ced2b6dee218610a0
HoJLter <hojlter.work@gmail.com>
2026-09-25 02:41:52 +0300

    Add: SFML initial state

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,25 @@
 ﻿#include "main.h"
+#include "SFML/Graphics.hpp"
 
 
 int main()
 {
-	std::cout << "Hello CMake." << std::endl;
-	return 0;
+    sf::RenderWindow window(
+        sf::VideoMode({ 800, 800 }),
+        "SFML"
+    );
+
+    while (window.isOpen())
+    {
+        while (auto event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
+
+    return 0;
 }
